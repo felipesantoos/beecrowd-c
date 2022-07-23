8bd82a3d82dce0032f325d6cc16ae03c78e3870f
felipesantoos <felipedssantos811@gmail.com>
2022-07-23 14:37:14 -0300

    Add difference code (#8)

---
+++ difference.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int A, B, C, D;
+    scanf("%d%d%d%d", &A, &B, &C, &D);
+
+    int DIFF = (A * B) - (C * D);
+    printf("DIFERENCA = %d\n", DIFF);
+
+    return 0;
+}
